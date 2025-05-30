e7218fbdf04ccf0aafde4e1f0a178cb0e2487201
dodoezt <aldofirmansyah579@gmail.com>
2025-05-30 11:37:28 +0700

    update 30/5/2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 import ChatSearchBar from '@/components/seperated-component/chat/searchBar'
 import ChatNavbar from '@/components/seperated-component/chat/navbar'
 import LogoLoading from '@/components/loadings/logoLoading'
 import { useUnifiedAuth } from '@/components/contexts/parents/authProvider'
 
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoCheckmarkDoneOutline, IoCheckmark } from "react-icons/io5";
+
+const VALID_TABS = ['chat', 'search']
+const DEFAULT_TAB = 'chat'
+
 const page = () => {
   const auth = useUnifiedAuth();
   const [loading, setLoading] = useState<boolean>(true)
+  const [isSearchOnFocus, setIsSearchOnFocus] = useState<boolean>(false)
 
+  const searchParams = useSearchParams()
   const router = useRouter()
-
-  //NOTE : PERBAIKI SETELAH LOGIN LANGSUNG GANTI PROVIDER
 
   const { provider } = auth;
 
@@ -25,7 +31,37 @@ const page = () => {
     }
   }, [provider])
 
-  if (loading) {
+  const tab = searchParams.get('tab')
+
+  useEffect(() => {
+    if(!tab || !VALID_TABS.includes(tab)){
+      const params = new URLSearchParams(searchParams)
+      params.set('tab', DEFAULT_TAB)
+      router.replace(`?${params.toString()}`)
+    }
+
+    if(tab === 'search'){
+      setIsSearchOnFocus(true)
+    }
+  }, [])
+
+  const handleSearchFocus = () => {
+    const params = new URLSearchParams(searchParams)
+    params.set('tab', 'search')
+    router.replace(`?${params.toString()}`)
+    setIsSearchOnFocus(true)
+  }
+
+  const handleSearchBlur = () => {
+    const params = new URLSearchParams(searchParams)
+    params.set('tab', DEFAULT_TAB)
+    router.replace(`?${params.toString()}`)
+    setIsSearchOnFocus(false)
+  }
+
+  //NOTE : PERBAIKI SETELAH LOGIN LANGSUNG GANTI PROVIDER
+
+  if (loading || !tab) {
     return (
       <div className="w-screen h-screen">
         <LogoLoading />
@@ -34,13 +70,60 @@ const page = () => {
   }
 
   return (
-    <div className='w-screen h-screen'>
-      <header className="w-full px-4 py-3 flex items-center justify-between border-b border-b-[#2c2c2c]">
+    <div className='w-screen h-screen relative'>
+      <header className={`w-full px-4 py-3 items-center justify-between border-b border-b-[#2c2c2c]
+      ${isSearchOnFocus ? 'hidden' : 'flex'}  
+      `}>
         <ChatNavbar />
       </header>
+      <div className={`w-full px-3 
+                      ${isSearchOnFocus && 'absolute top-2 left-1/2 -translate-x-1/2'}
+                    `}>
+        <ChatSearchBar handleSearchFocus={handleSearchFocus} handleSearchBlur={handleSearchBlur} isSearchOnFocus={isSearchOnFocus}/>
+      </div>
       <main className="w-full">
-        <div className="w-full px-5">
-          <ChatSearchBar />
+        <div className="w-full h-16 px-3 py-2 flex items-center justify-between">
+          <div className="h-full flex items-center space-x-2">
+            <div className="aspect-square h-full rounded-full flex items-center justify-center">
+              <FaRegCircleUser className='text-5xl text-[#e0e0e0]'/>
+            </div>
+            <div className="flex flex-col h-full content-between">
+              <h1 className="font-sans font-medium text-[#e0e0e0] text-sm">dodo ganteng</h1>
+              <p className="font-sans font-normal text-[#888888] text-xs flex"><span className=""><IoCheckmarkDoneOutline className='text-lg text-[#888888]'/></span> woi jawajawa</p>
+            </div>
+          </div>
+          <div className="h-full flex items-start">
+            <p className="font-sans font-light text-[#888888] text-xs">11:24 AM</p>
+          </div>
+        </div>
+        <div className="w-full h-16 px-3 py-2 flex items-center justify-between">
+          <div className="h-full flex items-center space-x-2">
+            <div className="aspect-square h-full rounded-full flex items-center justify-center">
+              <FaRegCircleUser className='text-5xl text-[#e0e0e0]'/>
+            </div>
+            <div className="flex flex-col h-full content-between">
+              <h1 className="font-sans font-medium text-[#e0e0e0] text-sm">sayang</h1>
+              <p className="font-sans font-normal text-[#888888] text-xs flex"><span className=""><IoCheckmarkDoneOutline className='text-lg text-cyan-400'/></span> iahh bub</p>
+            </div>
+          </div>
+          <div className="h-full flex items-start">
+            <p className="font-sans font-light text-[#888888] text-xs">11:24 AM</p>
+          </div>
+        </div>
+        <div className="w-full h-16 px-3 py-2 flex items-center justify-between">
+          <div className="h-full flex items-center space-x-2">
+            <div className="aspect-square h-full rounded-full flex items-center justify-center">
+              <FaRegCircleUser className='text-5xl text-[#e0e0e0]'/>
+            </div>
+            <div className="flex flex-col h-full content-between">
+              <h1 className="font-sans font-medium text-[#e0e0e0] text-sm">Kairi (Rival)</h1>
+              <p className="font-sans font-normal text-[#888888] text-xs">-1 jungler do </p>
+            </div>
+          </div>
+          <div className="h-full flex flex-col items-start justify-end gap-2">
+            <p className="font-sans font-light text-[#888888] text-xs">11:24 AM</p>
+            <div className="aspect-square rounded-full bg-green-400 flex items-center justify-center text-xs font-medium">1</div>
+          </div>
         </div>
       </main>
     </div>
--- src/components/seperated-component/chat/searchBar.tsx
+++ src/components/seperated-component/chat/searchBar.tsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import { CiSearch } from "react-icons/ci";
+import { IoArrowBack } from "react-icons/io5";
 
-const ChatSearchBar = () => {
+type props = {
+  handleSearchFocus: () => void
+  handleSearchBlur: () => void
+  isSearchOnFocus: boolean;
+}
+
+const ChatSearchBar:React.FC<props> = 
+  ({
+    handleSearchFocus,
+    handleSearchBlur, 
+    isSearchOnFocus
+  }) => {
+
+  const searchInputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if(isSearchOnFocus && searchInputRef.current){
+      searchInputRef.current.focus()
+    }
+  }, [])
+  
   return (
     <div className='w-full h-14 py-2 relative flex items-center justify-center'>
-        <input 
-        type="text" 
-        className="appearance-none outline-none flex-1 h-full pl-4 border-y border-l border-[#2c2c2c] bg-transparent rounded-l-full font-sans text-[#e0e0e0] font-normal text-sm"
-        placeholder='What are you looking for?'
-        />
+        <div className="h-full flex-1 flex items-center">
+
+          {isSearchOnFocus && (
+            <div className="aspect-square h-full flex items-center justify-center">
+              <button 
+              className="w-full h-full text-lg text-[#e0e0e0] flex items-center justify-center cursor-pointer"
+              onClick={handleSearchBlur}
+              >
+                <IoArrowBack />
+              </button>
+            </div>
+          )}
+
+          <input 
+          ref={searchInputRef}
+          type="text" 
+          className="appearance-none outline-none flex-1 h-full pl-4 border-y border-l border-[#2c2c2c] bg-transparent rounded-l-full font-sans text-[#e0e0e0] font-normal text-sm"
+          placeholder='What are you looking for?'
+          onFocus={handleSearchFocus}
+          onBlur={handleSearchBlur}
+          />
+        </div>
         <div className="aspect-square w-auto h-full flex items-center justify-center">
             <button 
             className="aspect-square h-full pr-4 text-xl text-[#e0e0e0] border-y border-r border-[#2c2c2c] rounded-r-full flex items-center justify-center cursor-pointer">
