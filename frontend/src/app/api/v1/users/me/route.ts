bd138810633ed2b37c5ed643df5e19decf327b4f
dodoezt <aldofirmansyah579@gmail.com>
2025-07-18 20:25:59 +0700

    feat: improve api to support new redis caching system

--- frontend/src/app/api/v1/users/me/route.ts
+++ frontend/src/app/api/v1/users/me/route.ts
@@ -1,18 +1,20 @@
 import { GetUserInfoFromCookie } from '@/lib/auth/getUserInfoFromCookie';
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
-import { UserCache } from '@/lib/chaches/UserCache';
+import { UserCache } from '@/lib/caches/UserCache';
+import { redis } from "@/lib/caches/RedisCaches";
 
 export async function POST (req: Request) {
     const userInfo = await GetUserInfoFromCookie()
     const email = userInfo?.email;
 
-    if(!email) return NextResponse.json({message: 'invalid email'}, {status: 400})
+    if(!email) return NextResponse.json({user: null, message: 'invalid email'}, {status: 400})
 
-    // const cache = UserCache.get('user')
-    // if(cache) {
-    //     console.log('cache:', cache)
-    //     return NextResponse.json({user: cache, message: 'cache provided'}, {status: 200})
+    // const cachedUser: any = redis.get(`user:${userInfo.userId}`);
+
+    // if (cachedUser) {
+    //     console.log('User found in cache:', cachedUser);
+    //     return NextResponse.json({ user: cachedUser }, { status: 200 });
     // }
     
     try {
@@ -28,13 +30,15 @@ export async function POST (req: Request) {
             },
         })
 
-        if (!user) return NextResponse.json({message : 'user not found'}, {status: 404})
+        if (!user) return NextResponse.json({user: null, message : 'user not found'}, {status: 404})
             
-        UserCache.set('user', user)
+        // await redis.set(`user:${userInfo.userId}`, JSON.stringify(user), {
+        //     ex: 60 * 60 * 24,
+        // })
 
         return NextResponse.json({user: user}, {status: 200})
     } catch (error) {
-        return NextResponse.json({message: 'internal server error'}, {status: 500})
+        return NextResponse.json({user:null, message: 'internal server error'}, {status: 500})
     }
 
 }
