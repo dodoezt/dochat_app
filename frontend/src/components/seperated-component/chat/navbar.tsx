31c53c9ce45ca8f8b808ce6ef438a71b73385082
dodoezt <aldofirmansyah579@gmail.com>
2025-08-21 10:29:14 +0700

    feat: add menu for navigation in navbar

--- frontend/src/components/seperated-component/chat/navbar.tsx
+++ frontend/src/components/seperated-component/chat/navbar.tsx
@@ -1,17 +1,105 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useRef, act } from 'react'
+import { useRouter } from 'next/navigation'
+import { UserInfoType } from '@/types/user'
+import { UseBoolean } from '@/hooks/useBoolean'
 
-const ChatNavbar = () => {
+import { MdAccountCircle } from 'react-icons/md'
+import { 
+    FaUser,
+    FaUserFriends,
+} from "react-icons/fa";
+import { 
+    IoMdNotifications,
+    IoMdSettings
+} from "react-icons/io";
+
+const menuItems = [
+    {
+        name: 'Profile',
+        icon: <FaUser className='text-base text-white'/>,
+        navigate: '/profile'
+    },
+    {
+        name: 'Friends',
+        icon: <FaUserFriends className='text-lg text-white'/>,
+        navigate: '/friends'
+    },
+    {
+        name: 'Notifications',
+        icon: <IoMdNotifications className='text-lg text-white'/>,
+        navigate: '/notifications'
+    },
+    {
+        name: 'Settings',
+        icon: <IoMdSettings className='text-lg text-white'/>,
+        navigate: '/settings'
+    },
+]
+
+const ChatNavbar = ({userInfo} : {userInfo: UserInfoType}) => {
+    const isMenuOpen = UseBoolean(false);
+    const isMenuClicked = UseBoolean(false);
+    const menuRef = useRef<HTMLDivElement>(null);
+    const router = useRouter()
+
+    function handleMenuToggle() {
+        if(isMenuClicked.value) return;
+        isMenuClicked.setTrue();
+
+        const toggledValue = !isMenuOpen.value;
+        isMenuOpen.toggle();
+        if(toggledValue) {
+            menuRef.current!.classList.replace('hidden', 'flex');
+            setTimeout(() => {
+                menuRef.current!.classList.replace('opacity-0', 'opacity-100');
+                menuRef.current!.classList.replace('-translate-y-5', '-translate-y-0');
+                isMenuClicked.setFalse();
+            }, 100)
+        } else {
+            menuRef.current!.classList.replace('opacity-100', 'opacity-0');
+            menuRef.current!.classList.replace('-translate-y-0', '-translate-y-5');
+            setTimeout(() => {
+                menuRef.current!.classList.replace('flex', 'hidden');
+                isMenuClicked.setFalse();
+            }, 200)
+        }
+    }
+
     return (
         <>
-            <div className="">
-                <img src="/assets/doChat.svg" alt="doChat-logo" className="w-auto h-9 bg-[#e0e0e0] px-2 rounded-full" />
-            </div>     
-            <div className="">
-                <button className="aspect-square flex flex-col items-center justify-center space-y-[3px]">
-                    <div className="aspect-square w-auto h-[4px] rounded-full bg-[#e0e0e0]"></div>
-                    <div className="aspect-square w-auto h-[4px] rounded-full bg-[#e0e0e0]"></div>
-                    <div className="aspect-square w-auto h-[4px] rounded-full bg-[#e0e0e0]"></div>
+            <button 
+            onClick={() => handleMenuToggle()}
+            id='overlay' 
+            className= {`${isMenuOpen.value ? 'block' : 'hidden'} w-screen h-screen pointer-events-auto z-20 fixed top-0 left-0`}></button>       
+            <h1 className="font-roboto text-[#e0e0e0] text-xl font-medium">YipYap</h1>
+            <div className="relative flex items-center justify-center">
+                <button
+                onClick={handleMenuToggle}  
+                className="flex items-center justify-center overflow-hidden rounded-full cursor-pointer h-9 aspect-square">
+                    {userInfo?.user_atribut.pfp_id ? (
+                        <img src={`${process.env.NEXT_PUBLIC_APPWRITE_BUCKET_URL}${userInfo?.user_atribut.pfp_id}${process.env.NEXT_PUBLIC_APPWRITE_BUCKET_URL_END}`} alt={`${userInfo.username} pfp`} className='w-full h-full' />
+                    ) : (
+                        <div className="flex items-center justify-center h-full aspect-square">
+                            <MdAccountCircle className='text-4xl text-white'/>
+                        </div>
+                    )}
                 </button>
+                <div id='menu' ref={menuRef} className='absolute right-[50%] w-56 transition-all duration-200 ease-in-out bg-[#121212] top-[110%] z-[30] hidden flex-col opacity-0 rounded-b-2xl rounded-tl-2xl border border-[#2c2c2c] -translate-y-5'>
+                    {menuItems.map((item, idx) => {
+                        return (
+                            <button
+                            onClick={() => {
+                                router.push(item.navigate);
+                                isMenuOpen.setFalse();
+                            }}
+                            key={idx}
+                            className="flex items-center w-full h-10 py-2 px-3 gap-2 p-2 hover:bg-[#1a1a1a] cursor-pointer">
+                                {item.icon}
+                                <span className='font-sans text-sm font-normal text-white'>{item.name}</span>
+                            </button>
+                        )
+                    })}
+                </div>
             </div>
         </>
     )
